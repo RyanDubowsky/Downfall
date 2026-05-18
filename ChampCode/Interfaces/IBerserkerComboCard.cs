71d38fa89709223f2f8e4b7b2d831dda16cf3f75
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-18 13:07:05 +0800

    refactor(ChampCardModel): add combo & glory
    Implement `IBerserkerComboCard` or `IDefensiveComboCard` to
    automatically add the tooltips & conditional logics.
    
    Use `WithGlory` to properly add `GloryPower` and
    `ChampTip.Ultimate`.

---
+++ ChampCode/Interfaces/IBerserkerComboCard.cs
@@ -0,0 +1,10 @@
+
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace Champ.ChampCode.Interfaces;
+
+public interface IBerserkerComboCard
+{
+    public Task BerserkerComboEffect(PlayerChoiceContext ctx, CardPlay cardPlay);
+}
