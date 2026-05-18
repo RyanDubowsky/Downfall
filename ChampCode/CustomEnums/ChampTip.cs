935d1b1ae0e95a7f64806c769259852efcf887d7
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-18 13:13:23 +0800

    feat(ChampTip): add new tooltips
    `ComboBonus` is unnecessary since it's able to use
    `ChampKeyword.TriggerSkillBonus` to display the same tooltip.
    
    `Finisher` is necessary because `ChampTag.Finisher` can't provide any
    tooltip information.

--- ChampCode/CustomEnums/ChampTip.cs
+++ ChampCode/CustomEnums/ChampTip.cs
@@ -5,5 +5,10 @@ namespace Champ.ChampCode.CustomEnums;
 
 public static class ChampTip
 {
+    [CustomEnum] public static StaticHoverTip Berserker;
+    [CustomEnum] public static StaticHoverTip Combo;
+    [CustomEnum] public static StaticHoverTip Defensive;
     [CustomEnum] public static StaticHoverTip Finisher;
+    [CustomEnum] public static StaticHoverTip Stance;
+    [CustomEnum] public static StaticHoverTip Ultimate;
 }
