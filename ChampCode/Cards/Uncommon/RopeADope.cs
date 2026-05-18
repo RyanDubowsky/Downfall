0c04fc10f7049e5c130b627d6d2c23ba7c3cd47b
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-18 15:57:27 +0800

    fix(RopeADope): only Energy can use energyIcons()

--- ChampCode/Cards/Uncommon/RopeADope.cs
+++ ChampCode/Cards/Uncommon/RopeADope.cs
@@ -13,16 +13,14 @@ public class RopeADope : ChampCardModel
     {
         WithFinisher();
         WithBlock(8, 2);
-        WithPower<EnergyNextTurnPower>(1, 1, false);
+        WithEnergy(1, 1);
         WithPower<DrawCardsNextTurnPower>(2, false);
-        WithEnergyTip();
-        WithCards(1);
     }
 
     protected override async Task PlayEffect(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
         await CommonActions.CardBlock(this, cardPlay);
-        await CommonActions.ApplySelf<EnergyNextTurnPower>(ctx, this);
+        await CommonActions.ApplySelf<EnergyNextTurnPower>(ctx, this, DynamicVars.Energy.BaseValue);
         await CommonActions.ApplySelf<DrawCardsNextTurnPower>(ctx, this);
     }
 }
