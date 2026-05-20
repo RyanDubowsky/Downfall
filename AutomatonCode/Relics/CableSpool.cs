18d148c4a8d69ab4dba1e4d5dbb1da70d27a7b08
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-20 19:24:01 +0800

    feat(CableSpool): implement

--- AutomatonCode/Relics/CableSpool.cs
+++ AutomatonCode/Relics/CableSpool.cs
@@ -1,11 +1,41 @@
 using Automaton.AutomatonCode.Core;
+using Automaton.AutomatonCode.Events;
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 
 namespace Automaton.AutomatonCode.Relics;
 
 [Pool(typeof(AutomatonRelicPool))]
-public class CableSpool() : AutomatonRelicModel(RelicRarity.Uncommon)
+public class CableSpool : AutomatonRelicModel, IOnEncode
 {
-    // TODO
+    private bool _usedThisCombat = false;
+    public CableSpool() : base(RelicRarity.Uncommon)
+    {
+
+    }
+
+    public override Task BeforeCombatStart()
+    {
+        _usedThisCombat = false;
+        Status = RelicStatus.Active;
+        return Task.CompletedTask;
+    }
+
+
+    public async Task OnCardEncoded(PlayerChoiceContext ctx, CardModel encodedCard, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Owner != Owner || _usedThisCombat)
+            return;
+        Flash();
+        _usedThisCombat = true;
+        Status = RelicStatus.Normal;
+
+        var copy = encodedCard.CreateClone();
+        await CardPileCmd.AddGeneratedCardToCombat(copy, PileType.Hand, Owner);
+        await AutomatonCmd.EncodeCard(copy, ctx, cardPlay);
+    }
 }
