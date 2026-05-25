e56a0702c8590d39f9e4a81c80b177d2e1180628
Tsukimaru Oshawott <cutekibry@gmail.com>
2026-05-26 02:32:39 +0800

    fix(Signature): should make cost = 0

--- ChampCode/Enchantments/Signature.cs
+++ ChampCode/Enchantments/Signature.cs
@@ -1,5 +1,6 @@
 ﻿using Champ.ChampCode.CustomEnums;
 using Downfall.DownfallCode.Abstract;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Models;
 
 namespace Champ.ChampCode.Enchantments;
@@ -9,5 +10,9 @@ public class Signature : DownfallEnchantmentModel<Core.Champ>
     public override bool CanEnchant(CardModel card)
     {
         return card.Tags.Contains(ChampTag.Finisher);
+    }
+    protected override void OnEnchant()
+    {
+        Card.EnergyCost.UpgradeBy(-Card.EnergyCost.GetWithModifiers(CostModifiers.None));
     }
 }
